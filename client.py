a47f95ca6bf75e7f147f33985dc89c509b587964
poojya <109895024+POOJYADODDAPPAAPPA@users.noreply.github.com>
2022-07-26 04:33:39 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,16 @@
+import socket
+
+hostName = socket.gethostname()
+host = socket.gethostbyname(hostName)
+port = 1234
+
+client_socket = socket.socket()
+client_socket.connect((host, port))
+
+msg = input("Enter msg:")
+
+while msg.lower().strip() != 'bye':
+	client_socket.send(msg.encode())
+	data = client_socket.recv(1024).decode()
+	print("Received from server:",data)
+	msg = input("Enter msg to send to server:")
